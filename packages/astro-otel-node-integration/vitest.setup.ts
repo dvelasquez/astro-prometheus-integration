4a12e9c233b2837889d7324626d439177cf3c0bd
Danilo Velasquez <i@d13z.dev>
2025-09-05 20:12:09 +0200

    feat: added initial code for otel

---
+++ packages/astro-otel-node-integration/vitest.setup.ts
@@ -0,0 +1,13 @@
+console.log("Vitest setup file loaded.");
+
+// Define a default structure for the global options before any tests run
+// globalThis.__PROMETHEUS_OPTIONS__ = {
+// 	metricsUrl: "/metrics",
+// 	registerContentType: "PROMETHEUS",
+// 	enabled: true,
+// 	standaloneMetrics: {
+// 		enabled: false,
+// 		port: 9090,
+// 	},
+// 	collectDefaultMetricsConfig: undefined, // Add other potential fields if needed
+// };
