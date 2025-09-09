1603613368e12eeabd9081ce373ff678e707d381
Danilo Velasquez <i@d13z.dev>
2025-09-09 06:51:11 +0200

    feat: refactor OpenTelemetry integration with new build scripts, improved configuration, and enhanced Docker support

--- packages/astro-opentelemetry-integration/dist/utils/constants.js
+++
@@ -1,8 +0,0 @@
-// src/utils/constants.ts
-var ALLOWED_ADAPTERS = ["@astrojs/node"];
-var INTEGRATION_NAME = "astro-opentelemetry-integration";
-export {
-  ALLOWED_ADAPTERS,
-  INTEGRATION_NAME
-};
-//# sourceMappingURL=constants.js.map
--- packages/astro-opentelemetry-integration/dist/utils/getAttributes.js
+++
@@ -1,8 +0,0 @@
-// src/utils/getAttributes.ts
-var OTEL_SERVICE_NAME = globalThis.__OTEL_OPTIONS__.serviceName || process.env.OTEL_SERVICE_NAME;
-var OTEL_SERVICE_VERSION = globalThis.__OTEL_OPTIONS__.serviceVersion || process.env.OTEL_SERVICE_VERSION;
-export {
-  OTEL_SERVICE_NAME,
-  OTEL_SERVICE_VERSION
-};
-//# sourceMappingURL=getAttributes.js.map
