772183a44cf0cfedf1c1887dca184699dcc21e14
Danilo Velasquez <i@d13z.dev>
2025-09-11 20:27:04 +0200

    tests: added otel prometheus e2e test

--- playgrounds/otel/playwright.config.ts
+++ playgrounds/otel/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: "http://localhost:4321",
+		baseURL: "http://localhost:8000",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: "on-first-retry",
@@ -78,8 +78,8 @@ export default defineConfig({
 
 	/* Run your local preview server before starting the tests */
 	webServer: {
-		command: "npm run preview",
-		url: "http://localhost:4321",
+		command: "pnpm preview",
+		url: "http://localhost:8000",
 		reuseExistingServer: !process.env.CI,
 		timeout: 120 * 1000, // 2 minutes timeout for server startup
 	},
